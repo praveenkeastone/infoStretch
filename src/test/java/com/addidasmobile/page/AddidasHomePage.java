0448dc07fc01e5caa962e4a33a54e00d1d91fae0
praveenvishu <praveenvishu973@gmail.com>
2018-01-22 15:23:49 +0530

    sdfdsfs

--- src/test/java/com/addidasmobile/page/AddidasHomePage.java
+++ src/test/java/com/addidasmobile/page/AddidasHomePage.java
@@ -55,6 +55,7 @@ public class AddidasHomePage extends WebDriverBaseTestPage<WebDriverTestPage   >
 		NaviagationTestPage nav= new NaviagationTestPage();
 		cart.verifyProductOnCartPage();
 		nav.getBackToHomePage();
+		System.out.println("trest");
 		
 		
 	}
